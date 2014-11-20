979aacf9c57164351fe269921467f7deac1d2b5a
Nandan Rao <nandanmarkrao@gmail.com>
2014-11-19 22:06:54 -0500

    just cleaned up formatting of things a little bit

--- instaConfig.js
+++ instaConfig.js
@@ -1,7 +1,5 @@
 var ig = require('instagram-node').instagram()
 
-var exports = {};
-
 var instaId = 'b7d3fb798edc4f478b5c9e3de061190d';
 var instaSecret = '560af6cf2ced451ea5efd41ec009393e';
 
--- pusher.js
+++ pusher.js
@@ -7,14 +7,17 @@ var instaId = require('./instaConfig').instaId
     , ig = require('./instaConfig').ig
 
 module.exports = function(session){
-
-  var registerValidationRouter = function(session){
+  
+  // Instagram makes a get request to this router when you 
+  // register a realtime channel
+  var createValidationRouter = function(session){
     router.get(session.route, function(req, res, next){
       res.send(req.query['hub.challenge'])
     })
     return Promise.resolve(session)
   }
 
+  // Creates an instagram 'location' object from a Lat/Lng
   var addLocation = function(session){
     return new Promise(function(resolve, reject){
       ig.add_geography_subscription(session.lat, session.lng, 5000, session.url, function(err, result, remaining, limit){
@@ -29,6 +32,7 @@ module.exports = function(session){
     });
   };
 
+  // What to do when a client disconnects
   var registerDisconnect = function(session){
     session.socket.on('disconnect', function(){
       ig.del_subscription({id: session.result.id}, function(err, subscriptions){
@@ -38,34 +42,44 @@ module.exports = function(session){
     return session
   }
 
-  var makeStream = function(session){
+  // Creates url to make GET request to instagram for this location
+  var createGetUrl = function(){
+    return 'https://api.instagram.com/v1/geographies/' + 
+    session.location + 
+    '/media/recent?client_id=' + 
+    instaId;
+  }
 
-      var bus = new Bacon.Bus()
+  // Creates a stream from the instagram GET url
+  var getImages = function(){
+    return Bacon.fromNodeCallback(needle.get, createGetUrl())
+    .map(function(res){
+      return res.body.data
+    }) 
+  }
 
-      bus.onError(function(err){
+  // Creates our router to recieve post notifications on new images
+  var createPostRouter = function(session){
+    session.stream = new Bacon.Bus()
+    router.post(session.route, function(req, res, next){
+      session.stream.push('hit')
+      res.status(200).send('OK');
+    })
+    return Promise.resolve(session)
+  } 
+
+  // Run shit
+  createValidationRouter(session)
+    .then(addLocation)
+    .then(registerDisconnect)
+    .then(createPostRouter)
+    .then(function(session){
+      // Error handler goes here...
+      session.stream.onError(function(err){
         console.log('ERROR', err)  
-      }) 
-
-      router.post(session.route, function(req, res, next){
-        bus.push('hit')
-        res.status(200).send('OK');
-      }) 
-
-      var getUrl = function(){
-        return 'https://api.instagram.com/v1/geographies/' + 
-        session.location + 
-        '/media/recent?client_id=' + 
-        instaId;
-      }
-
-      var getImages = function(){
-        return Bacon.fromNodeCallback(needle.get, getUrl())
-        .map(function(res){
-          return res.body.data
-        }) 
-      }
-
-      bus
+      })
+      // Begin FRP magic
+      session.stream         
         .bufferingThrottle(2000) 
         .flatMap(getImages)
         .map(function(arr){
@@ -81,12 +95,5 @@ module.exports = function(session){
         .onValue(function(val){
           session.socket.emit('pic', val)
         }) 
-
-    }
-
-    // RUN!!!!
-    registerValidationRouter(session)
-      .then(addLocation)
-      .then(registerDisconnect)
-      .then(makeStream)
+    })
 }
